0d53421bf7db01b70111539845cbaedbd1bc5b85
Atahan Ekici <atahanekici@hotmail.com>
2023-02-07 19:52:00 +0300

    Started Working on Car Mechanics

--- Wrecking.io/Assets/Scripts/Car/CarController.cs
+++ Wrecking.io/Assets/Scripts/Car/CarController.cs
@@ -1,13 +1,51 @@
 using UnityEngine;
-
 public class CarController : MonoBehaviour
 {
-    void Start()
+    private static readonly string GroundTag = "Ground";
+
+    [Header("Fall Settings")]
+    [SerializeField] private float FallThreshold = -10f;
+
+    [Header("Local Componenet References")]
+    [SerializeField] private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start()
     {
         
     }
-    void Update()
+    private void FixedUpdate()
     {
         
+    }
+    private void Update()
+    {
+        CheckFall();
+    }
+    private void LateUpdate()
+    {
+        
+    }
+    private void CheckFall()
+    {
+        if(transform.position.y <= FallThreshold)
+        {
+            Debug.Log(gameObject.name + " named vehicle fell");
+            Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        Debug.Log(gameObject.name+" destroyed");
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag(GroundTag))
+        {
+            Debug.Log("Touched Ground");
+        }
     }
 }
